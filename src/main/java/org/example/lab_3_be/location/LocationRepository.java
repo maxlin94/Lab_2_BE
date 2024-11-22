47f2f66c4e73a0bc03db96ecba506989f541a364
Max Lindström <max941@live.se>
2024-11-23 00:21:40 +0100

    Changed to interface

--- src/main/java/org/example/lab_3_be/location/LocationRepository.java
+++ src/main/java/org/example/lab_3_be/location/LocationRepository.java
@@ -1,4 +1,7 @@
 package org.example.lab_3_be.location;
 
-public class LocationRepository {
+import org.example.lab_3_be.entities.Location;
+import org.springframework.data.repository.ListCrudRepository;
+
+public interface LocationRepository extends ListCrudRepository<Location, Long> {
 }
