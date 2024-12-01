c8f658a1753a7abddd757ec8a52e20666bd8af3b
Max Lindström <max941@live.se>
2024-12-01 12:17:47 +0100

    Added security config

---
+++ src/main/java/org/example/lab_3_be/security/SecurityConfig.java
@@ -0,0 +1,48 @@
+package org.example.lab_3_be.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.http.HttpMethod.*;
+
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize ->
+                        authorize
+                                .requestMatchers(GET, "/api/locations").permitAll()
+                                .requestMatchers(GET, "/api/categories", "/api/categories/*").permitAll()
+                                .requestMatchers(GET, "/api/locations/**").permitAll()
+                                .requestMatchers(PUT, "/api/locations/**").hasAuthority("USER")
+                                .requestMatchers(POST, "/api/locations").hasAuthority("USER")
+                                .requestMatchers(POST, "/api/categories").hasAuthority("ADMIN")
+                                .requestMatchers("/login", "/error", "/error/**", "/").permitAll()
+                                .anyRequest().denyAll())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
+        return http.build();
+    }
+
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("http://localhost:8080/roles");
+        grantedAuthoritiesConverter.setAuthorityPrefix("");
+
+        JwtAuthenticationConverter authenticationConverter = new JwtAuthenticationConverter();
+        authenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+        return authenticationConverter;
+    }
+}
