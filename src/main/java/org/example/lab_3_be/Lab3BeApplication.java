af418ae80e8a9a00f510953485d3e1edf7acf5dc
Max Lindström <max941@live.se>
2024-12-01 16:14:36 +0100

    Added .env for auth0 setup

--- src/main/java/org/example/lab_3_be/Lab3BeApplication.java
+++ src/main/java/org/example/lab_3_be/Lab3BeApplication.java
@@ -1,12 +1,19 @@
 package org.example.lab_3_be;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Lab3BeApplication {
 
+
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
+
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
         SpringApplication.run(Lab3BeApplication.class, args);
     }
 
