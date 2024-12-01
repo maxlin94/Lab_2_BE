cbc337e0a94d391db657406d5f0bde3f31b1f0f5
Max Lindström <max941@live.se>
2024-12-01 15:49:12 +0100

    Added name duplication check when adding category

--- src/main/java/org/example/lab_3_be/service/CategoryService.java
+++ src/main/java/org/example/lab_3_be/service/CategoryService.java
@@ -2,7 +2,9 @@ package org.example.lab_3_be.service;
 
 import org.example.lab_3_be.dto.CategoryDto;
 import org.example.lab_3_be.repository.CategoryRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -24,6 +26,9 @@ public class CategoryService {
     }
 
     public int addCategory(CategoryDto categoryDto) {
+        if(categoryRepository.findByName(categoryDto.name()) != null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Category with the same name already exists");
+        }
         var category = CategoryDto.toCategoryEntity(categoryDto);
         categoryRepository.save(category);
         return category.getId();
